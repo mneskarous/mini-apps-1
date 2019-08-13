3642165660c141b1f46a60036c77d80523f19130
Michael Eskarous <mneskarous@gmail.com>
2019-08-13 10:39:22 -0700

    Created package.json file and implemented server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
 
+const app = express();
+const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(express.static('client'));
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
+
+module.exports = app;
